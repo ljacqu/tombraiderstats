d15ad5316ddb87775c9e944ae24ba5dde4d098a3
ljacqu <ljacqu@users.noreply.github.com>
2015-07-13 18:33:45 +0200

    Created single-file aggregator, turned into modules. (Rough)

---
+++ aggregator.js
@@ -0,0 +1,404 @@
+var ljacqu = {};
+
+/* ---------------------------------------------------
+ * Configuration parameters 
+ * --------------------------------------------------- */
+ljacqu.config = function() {
+  var classesToAggregate = ['enemy', 'item', 'secret', 'gold', 'silver'];
+  
+  /** Custom plural to singular forms. */
+  var plurals = {
+    gunmen: 'gunman',
+    men: 'men',
+    mummies: 'mummy',
+    switches: 'switch',
+    tribesmen: 'tribesman',
+    wolves: 'wolf'
+  };
+  
+  return {
+    classesToAggregate: classesToAggregate,
+    plurals: plurals
+  };
+}();
+
+
+/* ---------------------------------------------------
+ * jQuery-related utility
+ * --------------------------------------------------- */
+ljacqu.jquery = function() {
+  /**
+   * Waits on jQuery to load. Based on
+   * <http://neighborhood.org/core/sample/jquery/append-to-head.htm>.
+   * @param {Callback} whenLoaded The function to execute once jQuery has 
+   *  loaded. This function is also called when jQuery did not have to be loaded
+   *  manually.
+   */
+  var loadJquery = function(whenLoaded) {
+    var attemptCount = 0;
+
+    var waitForLoadedJquery = function() {
+      attemptCount++;
+      if (typeof jQuery !== 'undefined') {
+        whenLoaded();
+        return;
+      }
+      if (attemptCount < 100) {
+        setTimeout(waitForLoadedJquery, 100);
+      } else {
+        console.error('Could not load jQuery!');
+      }
+    };
+
+    if (typeof jQuery === 'undefined') {
+      delete $;
+      var script = document.createElement('script');
+      script.src = 
+             "https://ajax.googleapis.com/ajax/libs/jquery/1.6.3/jquery.min.js";
+      document.getElementsByTagName('head')[0].appendChild(script);
+      waitForLoadedJquery();
+    } else {
+      whenLoaded();
+    }
+  };
+  return {
+    loadJquery: loadJquery
+  };
+}();
+
+
+/* ---------------------------------------------------
+ * Text transformations / semantic extraction
+ * --------------------------------------------------- */
+ljacqu.texthelper = function() {
+  /**
+   * Low-level sanitation: replaces "SECRET #3"-like occurrences to "secret" and
+   * removes any non-word characters (e.g. punctuation) from the beginning and
+   * the end.
+   * @param {String} extractedName The name to sanitize
+   * @returns {String} Sanitized version of the entity
+   */
+  var sanitizeEntityName = function(extractedName) {
+    if (extractedName.match(/^secret #\d+$/i)) {
+      return 'secret';
+    }
+    return extractedName.replace(/^\W+/, '').replace(/\W+$/, '')
+      .trim().toLowerCase();
+  };
+
+  /**
+   * Attempts to extract the number of entities such as when "2 bats" are
+   * mentioned.
+   * @param {String} extractedName The name in the HTML to process
+   * @returns {Object} number: number of occurrences;
+   *  name: cleaned entity name.
+   */
+  var getSemanticInfo = function(extractedName) {
+    extractedName = sanitizeEntityName(extractedName);
+    if (extractedName === '') {
+      return {name: '', number: 0};
+    }
+    // match "2 bats"
+    var matchNumber = extractedName.match(/^(\d+) (.*)$/);
+    if (matchNumber) {
+      return {
+        number: parseInt(matchNumber[1]),
+        name: matchNumber[2]
+      };
+    }
+    // match "three thugs"
+    var literalNumbers = ['one', 'two', 'three', 'four', 'five', 'six'];
+    var literalRegexp = new RegExp('^(' + literalNumbers.join('|') + ')' +
+      '\\s(.*?)$');
+    var literalMatch = extractedName.match(literalRegexp);
+    if (literalMatch) {
+      // push 0 to front so [1] -> one, [2] -> two, etc.
+      literalNumbers.unshift(0);
+      return {
+        name: literalMatch[2],
+        number: literalNumbers.indexOf(literalMatch[1])
+      };
+    }
+    // match the rest, e.g. "shark"
+    return {name: extractedName, number: 1};
+  };
+  
+  /**
+   * Replaces the plural with the singular form where applicable. Uses custom
+   * list defined in "plurals" but also merges entries such as "bats" and "bat" 
+   * into "bat" if the no-s ("bat") version exists.
+   * @param {String} entity the name of the entity
+   * @param {String} entityList the list of found entities
+   * @returns {String} the singular version of entity
+   */
+  var pluralToSingular = function(entity, entityList) {
+    entity = entity.toLowerCase();
+    if (typeof ljacqu.config.plurals[entity] !== 'undefined') {
+      return ljacqu.config.plurals[entity];
+    } else if (entity.substr(entity.length - 1) === 's') {
+      var singularName = entity.substr(0, entity.length - 1);
+      if (typeof entityList[singularName] !== 'undefined') {
+        return singularName;
+      }
+    }
+    return entity;
+  };
+  
+  /**
+   * Creates a regular expression to remove superfluous words in an entity, e.g.
+   * to match things like "a second thug" or "more bats".
+   * @returns {RegExp} pattern to recognize superfluous occurrences
+   */
+  var getCardinalRegexp = function() {
+    var cardinalWords = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth',
+      'seventh', 'eighth', 'ninth', 'tenth'];
+    var articles = ['the', 'a'];
+    var adverbs = ['more', 'additional', 'another', 'other', 'sets of', 
+      'boxes of', 'bunches of', 'bundles of'];
+    var makeRegexpOr = function(arr) {
+      return '(' + arr.join('\\s|') + '\\s)';
+    };
+    // (adverbs|(article? cardinal?)? (actualWord)
+    return new RegExp('^(' + makeRegexpOr(adverbs) + '|' +
+      makeRegexpOr(articles) + '?' +
+      makeRegexpOr(cardinalWords) + '?)?(.*?)$', 'i');
+  };
+
+  /**
+   * Merges entries of an object (entityName: totalNumber) where the entity name
+   * is the same, e.g. "more dogs", "a second dog" with "dog".
+   * @param {String} entity The entity name to verify (potentially merge).
+   * @param {Object} entityList The entities discovered as object, where
+   *  the key is the extracted name and the value the total number.
+   * @returns {Object} Merged object
+   */
+  var mergeEntityEntry = function(entity, entityList) {
+    var cardinalMatches = entity.match(getCardinalRegexp());
+    var noCardinalName = cardinalMatches ?
+      cardinalMatches[ cardinalMatches.length - 1 ] : entity;
+    noCardinalName = pluralToSingular(noCardinalName, entityList);
+    if (noCardinalName !== entity) {
+      console.log(entity + " renamed to " + noCardinalName);
+      if (typeof entityList[noCardinalName] !== 'undefined') {
+       entityList[noCardinalName] += entityList[entity];
+      } else {
+        entityList[noCardinalName] = entityList[entity];
+      }
+      delete entityList[entity];
+    }
+    return entityList;
+  };
+  
+  var mergeEntities = function(entityList) {
+    var mergedList = entityList;
+    for (var key in entityList) {
+      if (entityList.hasOwnProperty(key)) {
+        mergedList = mergeEntityEntry(key, mergedList);
+      }
+    }
+    return mergedList;
+  };
+  
+  return {
+    sanitizeEntityName: sanitizeEntityName,
+    getSemanticInfo: getSemanticInfo,
+    pluralToSingular: pluralToSingular,
+    mergeEntities: mergeEntities
+  };
+}();
+
+
+/* ---------------------------------------------------
+ * Document retrieval (HTML extraction, loading)
+ * --------------------------------------------------- */
+ljacqu.document = function() {
+  /**
+   * Extracts all entities from a document and returns their total.
+   * @param {String} clazz The CSS class to review
+   * @param {?Document} source The document (jQuery context) to examine. Omit
+   *  for current document.
+   * @returns {Object} key = text of the entity, value = total occurrence of the
+   *  entity.
+   */
+  var fetchEntities = function(clazz, source) {
+    source = source || document;
+    var types = {};
+    // Specifying span allows us to use the same class on <td>, which looks cool
+    $.each($("span." + clazz, source), function() {
+      var entry = ljacqu.texthelper.getSemanticInfo($(this).text());
+      if (entry.name === '') {
+        return;
+      } else if (typeof types[entry.name] === 'undefined') {
+        types[entry.name] = entry.number;
+      } else {
+        types[entry.name] += entry.number;
+      }
+    });
+    
+    return ljacqu.texthelper.mergeEntities(
+      ljacqu.texthelper.mergeEntities(types)
+    );
+  };
+  
+  /**
+   * Extract all walkthroughs from a page (for table overviews).
+   * @param {Callback} processFn Function to process the loaded document (HTML),
+   *  taking the page URL as second argument.
+   * @returns {undefined}
+   */
+  var loadAllWalkthroughs = function(processFn) {
+    $.each($('[class^="walk-table"] a[href^="walks/"]'), function() {
+      ljacqu.display.setupDataContainer($(this));
+      var url = $(this).attr('href');
+
+      $.get(url, {}, function(data) {
+        processFn(data, url);
+      }, 'html');
+    });
+  };
+  
+  return {
+    fetchEntities: fetchEntities,
+    loadAllWalkthroughs: loadAllWalkthroughs
+  };
+}();
+
+
+/* ---------------------------------------------------
+ * Display of results
+ * --------------------------------------------------- */
+ljacqu.display = function() {
+  /**
+   * Adds rows to a given table based on an object. Left cell is the object's
+   * key, while the right cell is the object's value for each entry.
+   * @param {jQuery} table jQuery-selected table object to modify
+   * @param {Object} entityList The object to display in the table
+   * @param {type} clazz TODO ----------------
+   */
+  var addDataToTable = function(table, entityList, clazz) {
+    for (var key in entityList) {
+      if (entityList.hasOwnProperty(key)) {
+        table.append($('<tr>')
+          .append('<td class="' + clazz + '">' + key + '</td>' +
+          '<td style="text-align: right">' + entityList[key] + '</td>')
+        );
+      }
+    }
+  };
+  
+  var ucfirst = function(text) {
+    return text.charAt(0).toUpperCase() + text.substr(1);
+  };
+  
+  /**
+   * Displays the result for a single page
+   */
+  var displayEntities = function(entityList, clazz) {
+    var containerId = clazz + 'Total';
+    var container = $('#' + containerId);
+    if (container.length === 0) {
+      $('#LayoutDiv1').before('<div id="' + containerId + '"></div>');
+      container = $('#' + containerId);
+    } else {
+      container.html('');
+    }
+    container.hide();
+    container.html('<h2>' + ucfirst(clazz) + ' total</h2>' + 
+      '<table><th>Type</th><th>Total</th></table>');
+    var containerTable = container.find('table');
+    addDataToTable(containerTable, entityList, clazz);
+    container.fadeIn();
+  };
+  
+  /**
+   * Creates a container ID based on the the URL to a walkthrough page.
+   * @param {string} url
+   * @returns {string} ID based on shortened page name
+   */
+  var getContainerId = function(url) {
+    var folders = url.split('/');
+    return 'p' + folders[ folders.length - 1 ].split('.')[0];
+  };
+  
+  var setupDataContainer = function(elem) {
+    var containerId = getContainerId(elem.attr('href'));
+    var container = $('#' + containerId);
+
+    if (container.length === 0) {
+      $('#wrap').before('<div id="' + containerId + '"><h1>' + elem.text() + 
+        '</h1></div>');
+      container = $('#' + containerId);
+      $('#' + containerId).find('h1').click(function() {
+        container.find('h2, table').toggle();
+      });
+    }
+  };
+
+  var displayDataInContainer = function(totalData, clazz, containerId) {
+    var container = $('#' + containerId);
+    var sectionClass = 'p' + clazz;
+    var sectionSelector = '#' + containerId + ' .' + sectionClass;
+
+    var section = $(sectionSelector);
+    if (section.length === 0) {
+      container.append('<div class="' + sectionClass + '"></div>');
+      section = $(sectionSelector);
+    }
+    section.hide();
+    section.html('<h2>' + clazz.charAt(0).toUpperCase() + clazz.substr(1) + 
+      '</h2> <table></table> </div>');
+    addDataToTable(section.find('table'), totalData, clazz);
+    section.find('h2').click(function() {
+      section.find('table').toggle();
+      return true;
+    });
+    section.fadeIn();
+  };
+  
+  return {
+    addDataToTable: addDataToTable,
+    displayEntities: displayEntities,
+    displayDataInContainer: displayDataInContainer,
+    setupDataContainer: setupDataContainer,
+    getContainerId: getContainerId
+  };
+}();
+
+
+/* ---------------------------------------------------
+ * Entry points
+ * --------------------------------------------------- */
+ljacqu.run = function() {
+  var singlePageRunner = function() {
+    for (var i = 0; i < ljacqu.config.classesToAggregate.length; i++) {
+      var currentClass = ljacqu.config.classesToAggregate[i];
+      ljacqu.display.displayEntities(
+        ljacqu.document.fetchEntities(currentClass), currentClass);
+    }
+  };
+  
+  var processLoadedWalkthrough = function(data, containerId) {
+    for (var i = 0; i < ljacqu.config.classesToAggregate.length; i++) {
+      var currentClass = ljacqu.config.classesToAggregate[i];
+      var items = ljacqu.document.fetchEntities(currentClass, data);
+      ljacqu.display.displayDataInContainer(items, currentClass, containerId);
+    }
+  };
+  
+  var overviewPageRunner = function() {
+    ljacqu.document.loadAllWalkthroughs(
+      function (data, url) {
+        processLoadedWalkthrough(data, ljacqu.display.getContainerId(url));
+      });
+  };
+  
+  return {
+    singlePageRunner: singlePageRunner,
+    overviewPageRunner: overviewPageRunner
+  };
+}();
+
+ljacqu.jquery.loadJquery(function() {
+  //ljacqu.run.singlePageRunner();
+  ljacqu.run.overviewPageRunner();
+});
